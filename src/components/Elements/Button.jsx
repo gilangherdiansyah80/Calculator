012819e90b6381ca6aa71b2dd22877bd7ed113fc
gilangherdiansyah80 <gilangherdiansyah404@gmail.com>
2025-09-20 13:54:20 +0700

    fix project

--- src/components/Elements/Button.jsx
+++ src/components/Elements/Button.jsx
@@ -3,7 +3,7 @@ const Button = ({ children, onClick }) => {
   return (
     <button
       onClick={onClick}
-      className="bg-red-500 hover:bg-red-400 text-white font-bold w-20 h-20 rounded-full border-b-4 border-red-700 hover:border-red-500 text-4xl"
+      className="bg-red-500 hover:bg-red-400 text-white font-bold w-20 h-20 lg:w-15 lg:h-15 rounded-full border-b-4 border-red-700 hover:border-red-500 text-4xl"
     >
       {children}
     </button>
